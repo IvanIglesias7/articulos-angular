5f5b2373cea0b99c90b6ef1c0ee99c216357dd07
DESKTOP-IFH2L3N\csi20-iiglhez <csi20-iiglhez@colegioaltair.net>
2023-04-24 13:59:56 +0200

    integracion crud 1/2

--- src/app/listado-articulos/listado-articulos.component.ts
+++ src/app/listado-articulos/listado-articulos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Articulo } from '../articulo';
 import { ArticulosService } from '../Servicios/articulo.service';
+import { MatTableDataSource } from '@angular/material/table/table-data-source';
 
 @Component({
   selector: 'app-listado-articulos',
@@ -9,9 +10,27 @@ import { ArticulosService } from '../Servicios/articulo.service';
 })
 export class ListadoArticulosComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['familia', 'nombre', 'precio'];
+  
+  dataSource = new MatTableDataSource<Articulo>();
+
+  constructor(private articulosService: ArticulosService) { }
 
   ngOnInit(): void {
+    this.getArticulos();
+  }
+
+  getArticulos() {
+
+    this.articulosService.getArticulos().subscribe(data => {
+      this.dataSource.data = data.map(e => {
+        return {
+          familia: e.doc.data()['familia'],
+          nombre: e.doc.data()['nombre'],
+          precio: e.doc.data()['precio'],
+        } as Articulo;
+      })
+    });
   }
 
 }
